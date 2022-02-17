a3a721d302c57bdab1ea9a0f5aba80c63821237f
José Pedro <pedroosd28@gmail.com>
2022-02-17 00:00:17 -0300

    feat: adicionando estrutura do projeto

---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+import GlobalStyles from "../styles/globals"
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
