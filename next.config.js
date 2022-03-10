30007c2cc82fca2b9d84adaa4c3e755b90c19e68
José Pedro <pedroosd28@gmail.com>
2022-03-10 01:54:57 -0300

    build: adding jsconfig files to improve the code experience

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd
+  },
+  images: {
+    domains: ['']
+  },
+  env: {}
+})
