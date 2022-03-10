eac8bb893bc61dcba76feb5c2d2c70d19e3afeac
José Pedro <pedroosd28@gmail.com>
2022-03-10 01:43:55 -0300

    feat: adding hero component

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,21 @@
+import { PrimaryButton, SecondaryButton } from '@/components/Buttons'
+
+import * as S from "./styles";
+
+export default function Hero() {
+  return (
+    <S.HeroContent>
+      <S.Title>Olá, somos o Book Write.</S.Title>
+      <S.Subtitle>A plataforma de histórias mais amada do mundo</S.Subtitle>
+      <S.Description>
+        O Book Write conecta uma comunidade global de 90 milhões de 
+        leitores e escritores através do poder das histórias.
+      </S.Description>
+      <S.ButtonSection>
+        <PrimaryButton>Iniciar leitura</PrimaryButton>
+        <SecondaryButton>Começar a escrever</SecondaryButton>
+      </S.ButtonSection>
+
+    </S.HeroContent>
+  )
+}
