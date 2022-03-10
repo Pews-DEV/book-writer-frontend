65b3d31c46b3d03501cecc7d162716959f0b69fd
José Pedro <pedroosd28@gmail.com>
2022-03-10 01:51:23 -0300

    feat: adding link to the standard font

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -49,6 +49,10 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;700;900&display=swap"
             rel="stylesheet"
           />
+          <link 
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" 
+            rel="stylesheet"
+          /> 
         </Head>
         <body>
           <Main />
