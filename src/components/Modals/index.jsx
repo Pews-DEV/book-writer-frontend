a3edfd6f50e0a2ae0dc679abba7070b47ecafcdc
José Pedro <pedroosd28@gmail.com>
2022-04-07 03:05:25 -0300

    refactor: creating a base modal based on register modal

---
+++ src/components/Modals/index.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from 'react'
+import Modal from 'react-modal'
+
+import * as S from './styles'
+
+const customStyles = {
+  content: {
+    position: 'absolute',
+    margin: 'auto auto',
+    maxWidth: '400px',
+    height: '580px',
+    padding: '0 25px',
+
+    background: '#FFFFFF',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    borderRadius: '6px'
+  }
+}
+
+const BaseModal = (props) => {
+  function closeModal() {
+    props.setModalIsOpen(false)
+  }
+
+  useEffect(() => {
+    Modal.setAppElement('body')
+  }, [])
+
+  return (
+    <Modal
+      isOpen={props.modalIsOpen}
+      onRequestClose={closeModal}
+      contentLabel={props.contentLabel || 'Modal'}
+      style={customStyles}
+    >
+      <S.ModalClose onClick={closeModal}>X</S.ModalClose>
+      {props.children}
+    </Modal>
+  )
+}
+
+export default BaseModal
---
+++ src/components/Modals/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const ModalClose = styled.button`
+  position: absolute;
+  top: var(--spacing-2);
+  right: var(--spacing-3);
+`
