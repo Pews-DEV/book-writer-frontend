1781875495f06567d5b769798f29f9e89d078b55
Gabriel Duete <gabrielmonteiroduete@gmail.com>
2022-04-19 03:07:20 -0300

    feat: add prop showButton in password

--- src/components/Modals/RegisterModal/index.jsx
+++ src/components/Modals/RegisterModal/index.jsx
@@ -27,11 +27,11 @@ const RegisterModal = (props) => {
           <S.ModalLabel htmlFor="email">E-mail</S.ModalLabel>
           <Input name="email" type="text" />
           <S.ModalLabel htmlFor="password">Senha</S.ModalLabel>
-          <Input name="password" type="password" />
+          <Input name="password" type="password" showButton={true} />
           <S.ModalLabel htmlFor="confirm_password">
             Confirmar Senha
           </S.ModalLabel>
-          <Input name="confirm_password" type="password" />
+          <Input name="confirm_password" type="password" showButton={true} />
           <S.ModalButtom type="submit">Inscrever-se</S.ModalButtom>
         </form>
       </S.ModalBody>
